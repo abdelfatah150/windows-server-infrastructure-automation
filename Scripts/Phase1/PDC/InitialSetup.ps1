7cab9b2654abf7a865ff65744c037ea7a438bbc3
unknown <abdelfatahsamir7@.com>
2026-07-17 16:58:38 +0300

    test phase 1&2

--- Scripts/Phase1/PDC/InitialSetup.ps1
+++ Scripts/Phase1/PDC/InitialSetup.ps1
@@ -4,16 +4,12 @@ Write-Host "========== PDC Initial Setup =========="
 Set-NetConnectionProfile -NetworkCategory Private
 
 # Set IP and DNS for PDC
-$adapter = Get-NetAdapter | Where-Object Status -eq "Up" | Select-Object -First 1
-
-Get-NetIPAddress -InterfaceIndex $adapter.InterfaceIndex -AddressFamily IPv4 |
-    Remove-NetIPAddress -Confirm:$false -ErrorAction SilentlyContinue
+$adapter = Get-NetAdapter -Name "Ethernet 2"
 
 New-NetIPAddress `
     -InterfaceIndex $adapter.InterfaceIndex `
     -IPAddress "192.168.10.2" `
-    -PrefixLength 24 `
-    -DefaultGateway "192.168.10.1"
+    -PrefixLength 24
 
 Set-DnsClientServerAddress `
     -InterfaceIndex $adapter.InterfaceIndex `
@@ -26,8 +22,6 @@ Enable-PSRemoting -Force
 Enable-NetFirewallRule -DisplayGroup "File and Printer Sharing"
 
 # Set computer name to PDC
-Rename-Computer -NewName "PDC" -Force
+Rename-Computer -NewName "PDC" -Restart -Force
 
 Write-Host "Restarting computer..."
-
-Restart-Computer -Force
--- Scripts/Phase2/PDC/SetupDomain.ps1
+++ Scripts/Phase2/PDC/SetupDomain.ps1
@@ -1,12 +1,15 @@
 Write-Host "========== PDC Domain Setup Started =========="
 
+# Set local Administrator password
+$AdminPassword = ConvertTo-SecureString "V@grant2026!" -AsPlainText -Force
+Set-LocalUser -Name "Administrator" -Password $AdminPassword
+
 # Install Required Windows Features
 $features = @(
     "AD-Domain-Services",
     "DHCP",
     "DNS",
     "Web-Server",
-    "Hyper-V",
     "FS-Resource-Manager"
 )
 
@@ -21,14 +24,19 @@ foreach ($feature in $features)
 
 # Create DSRM Password
 $DSRMPassword = ConvertTo-SecureString `
-"V@grant2026!" `
--AsPlainText `
--Force
+    "V@grant2026!" `
+    -AsPlainText `
+    -Force
 
 # Create Active Directory Forest
 Install-ADDSForest `
--DomainName "FINAL.LOCAL" `
--DomainNetbiosName "FINAL" `
--SafeModeAdministratorPassword $DSRMPassword `
--InstallDNS `
--Force
+    -DomainName "FINAL.LOCAL" `
+    -DomainNetbiosName "FINAL" `
+    -SafeModeAdministratorPassword $DSRMPassword `
+    -InstallDNS `
+    -NoRebootOnCompletion `
+    -Force
+
+Write-Host "Restarting server..."
+
+Restart-Computer -Force
